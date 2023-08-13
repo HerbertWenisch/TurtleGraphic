04d143c3b4479eb7c83a08c3e61b6b942b554d1f
HerbertWenisch <herbert.wenisch@t-online.de>
2023-08-13 10:35:04 +0200

    Initial sharing of project

---
+++ Turtlegrafik.java
@@ -0,0 +1,16 @@
+import ch.aplu.turtle.*;
+
+public class Turtlegrafik{
+    private static Turtle john = new Turtle();
+    
+    public static void square(){
+       for(int i = 0; i < 4; i++){
+           john.forward(100);
+           john.left(90);
+       } 
+    }
+    
+    public static void necklace(){
+       // Your code!
+    }
+}
